0c2dc859d42d0770a34194c1778d2dbf1992d3ac
tianmath <tiantsoamathieu@gmail.com>
2026-08-13 17:34:08 +0300

    remove cors since the notes app uses vite proxy to validate same-origin policy

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const cors = require('cors');
 const app = express();
 
 let notes = [
@@ -28,7 +27,6 @@ const requestLogger = (request, response, next) => {
   next();
 };
 
-app.use(cors());
 app.use(express.json());
 app.use(requestLogger);
 app.use(express.static('dist'));
