f6f3d0f49dcceed48c12f89d29951fc052e4006b
tianmath <tiantsoamathieu@gmail.com>
2026-08-19 19:49:20 +0300

    reformat JSON output by redefining the toJSON method from the schema

--- index.js
+++ index.js
@@ -43,6 +43,14 @@ const noteSchema = new mongoose.Schema({
   important: Boolean,
 });
 
+noteSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
+
 const Note = mongoose.model('Note', noteSchema);
 
 app.get('/', (request, response) => {
