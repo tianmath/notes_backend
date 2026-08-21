47d6e59185ea1a0af090b75d3de763b174c699a7
tianmath <tiantsoamathieu@gmail.com>
2026-08-21 07:25:16 +0300

    move error handling codes into errorHanlder() middleware

--- index.js
+++ index.js
@@ -44,16 +44,13 @@ app.get('/api/notes', (request, response) => {
   });
 });
 
-app.get('/api/notes/:id', (request, response) => {
+app.get('/api/notes/:id', (request, response, next) => {
   Note.findById(request.params.id)
     .then((note) => {
       if (note) response.json(note);
       else response.status(404).end();
     })
-    .catch((error) => {
-      console.log(error);
-      response.status(400).send({ error: 'malformatted id' });
-    });
+    .catch((error) => next(error));
 });
 
 app.delete('/api/notes/:id', (request, response) => {
@@ -87,6 +84,18 @@ const unknownEndpoint = (request, response) => {
 };
 
 app.use(unknownEndpoint);
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' });
+  }
+
+  next(error);
+};
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
