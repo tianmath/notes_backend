ccd90fa15428fb0f78de4d08205437b0449b30d5
tianmath <tiantsoamathieu@gmail.com>
2026-09-03 11:50:05 +0300

    add entries to the test database and write related tests

--- mongo.js
+++ mongo.js
@@ -7,7 +7,7 @@ if (process.argv.length < 3) {
 
 const password = process.argv[2];
 
-const url = `mongodb+srv://fullstack:${password}@cluster0.se5we2g.mongodb.net/noteApp?appName=Cluster0`;
+const url = `mongodb+srv://fullstack:${password}@cluster0.se5we2g.mongodb.net/testNoteApp?retryWrites=true&w=majority&appName=Cluster0`;
 
 mongoose.set('strictQuery', false);
 
@@ -20,19 +20,24 @@ const noteSchema = new mongoose.Schema({
 
 const Note = mongoose.model('Note', noteSchema);
 
-// const note = new Note({
-//   content: 'HTML is easy',
-//   important: true,
-// });
+const note1 = new Note({
+  content: 'HTML is easy',
+  important: true,
+});
 
-// note.save().then((result) => {
-//   console.log('note saved!');
-//   mongoose.connection.close();
-// });
+const note2 = new Note({
+  content: 'CSS is hard',
+  important: true,
+});
 
-Note.find({ important: true }).then((result) => {
-  result.forEach((note) => {
-    console.log(note);
-  });
+Promise.all([note1.save(), note2.save()]).then(() => {
+  console.log('two notes saved!');
   mongoose.connection.close();
 });
+
+// Note.find({ important: true }).then((result) => {
+//   result.forEach((note) => {
+//     console.log(note);
+//   });
+//   mongoose.connection.close();
+// });
--- tests/note_api.test.js
+++ tests/note_api.test.js
@@ -1,3 +1,4 @@
+const assert = require('node:assert');
 const { test, after } = require('node:test');
 const mongoose = require('mongoose');
 const supertest = require('supertest');
@@ -10,6 +11,19 @@ test('notes are returned as json', async () => {
     .get('/api/notes')
     .expect(200)
     .expect('Content-Type', /application\/json/);
+});
+
+test('all notes are returned', async () => {
+  const response = await api.get('/api/notes');
+
+  assert.strictEqual(response.body.length, 2);
+});
+
+test('a specific note is within the returned notes', async () => {
+  const response = await api.get('/api/notes');
+
+  const contents = response.body.map((e) => e.content);
+  assert(contents.includes('HTML is easy'));
 });
 
 after(async () => {
