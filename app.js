6de5d107fd626c408bb8e8879290d45e013601fb
tianmath <tiantsoamathieu@gmail.com>
2026-09-09 21:21:10 +0300

    implement token-based authentication endpoint

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const config = require('./utils/config');
 const logger = require('./utils/logger');
 const middleware = require('./utils/middleware');
+const loginRouter = require('./controllers/login');
 const notesRouter = require('./controllers/notes');
 const usersRouter = require('./controllers/users');
 
@@ -23,6 +24,7 @@ app.use(express.static('dist'));
 app.use(express.json());
 app.use(middleware.requestLogger);
 
+app.use('/api/login', loginRouter);
 app.use('/api/notes', notesRouter);
 app.use('/api/users', usersRouter);
 
