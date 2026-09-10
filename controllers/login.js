7eaebd1999ccbd7b58140ad610988c24ac236f56
tianmath <tiantsoamathieu@gmail.com>
2026-09-10 10:32:10 +0300

    implement token expiration

--- controllers/login.js
+++ controllers/login.js
@@ -21,7 +21,10 @@ loginRouter.post('/', async (request, response) => {
     id: user._id,
   };
 
-  const token = jwt.sign(userForToken, process.env.SECRET);
+  // token expires in one hour
+  const token = jwt.sign(userForToken, process.env.SECRET, {
+    expiresIn: 3600,
+  });
 
   response
     .status(200)
