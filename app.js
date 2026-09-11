c7f59033a04d16ca480c8a09471321a1b27cda54
tianmath <tiantsoamathieu@gmail.com>
2026-09-11 11:19:52 +0300

    refactor as to extract into a separate middleware the function responsible for getting the token from the Authorization header

--- app.js
+++ app.js
@@ -23,6 +23,7 @@ mongoose
 app.use(express.static('dist'));
 app.use(express.json());
 app.use(middleware.requestLogger);
+app.use(middleware.tokenExtractor);
 
 app.use('/api/login', loginRouter);
 app.use('/api/notes', notesRouter);
--- controllers/notes.js
+++ controllers/notes.js
@@ -18,18 +18,10 @@ notesRouter.get('/:id', async (request, response) => {
   }
 });
 
-const getTokenFrom = (request) => {
-  const authorization = request.get('authorization');
-  if (authorization && authorization.startsWith('Bearer ')) {
-    return authorization.replace('Bearer ', '');
-  }
-  return null;
-};
-
 notesRouter.post('/', async (request, response) => {
   const body = request.body;
 
-  const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET);
+  const decodedToken = jwt.verify(request.token, process.env.SECRET);
   if (!decodedToken.id) {
     return response.status(401).json({ error: 'token invalid' });
   }
